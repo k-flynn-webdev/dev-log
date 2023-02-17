828584819daeb957696f7de92ebe6e44f2d655f4
kevinflynn <flynny85@gmail.com>
2023-02-17 11:10:10 +0000

    add early form input to store

--- client/src/components/LogInput.js
+++ client/src/components/LogInput.js
@@ -7,27 +7,44 @@ import { update } from '../store/slices/logInput'
 
 // todo: Make these a function that randomises and encourages
 const PLACE_HOLDER = "Todays Milestone?"
+const LOG_INPUT_MIN_LENGTH = 10;
 
 function LogInput() {
-  const handleChange = (event) => dispatch(update(event.target.value))
-
-	const value = useSelector(state => state.logInput.value)
+ 	const value = useSelector(state => state.logInput.value)
 	const dispatch = useDispatch()
+
+ const handleChange = (event) => dispatch(update(event.target.value));
+  const handleSubmit = (event) => {
+  	event.preventDefault();
+  	console.log('user submitted', value)
+  }
+
 
 	return (
 		<Box>
-
-			<p>{value}</p>
-
-			<Input
-				type="text"
-				placeholder={PLACE_HOLDER}
-				value={value}
-        onChange={handleChange}
-			/>
-
+			<div>
+				<form id='logForm'
+							onSubmit={handleSubmit}
+							className='flex flex-row flex-wrap'
+				>
+					<Input 	id='logInput'
+								 	width={'15rem'}
+									className='mr-1 mb-2'
+									type='text'
+									placeholder={PLACE_HOLDER}
+									isRequired
+									minLength={LOG_INPUT_MIN_LENGTH}
+									onChange={handleChange}
+									value={value}
+					/>
+					<Button type='submit'
+									className='mb-2'>
+						Add
+					</Button>
+				</form>
+			</div>
 		</Box>
-	);
+	)
 }
 
 export default LogInput;

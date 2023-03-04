bb2e04f228ebf20f6735c731ae885a8e0fdd5e76
kevinflynn <flynny85@gmail.com>
2023-03-04 09:46:12 +0000

    new consts

--- client/src/lang/en-gb.js
+++ client/src/lang/en-gb.js
@@ -18,18 +18,27 @@ export const LOG_PLACEHOLDERS = [
 		"A new accomplishment?",
 		"Today's progress?",
 		"Today's completed task?",
+		"Today's goal?",
+		"Today's success?",
+		"What's new?",
 ];
 
 export const LANGUAGES = [
 		'python',
 		'bash',
 		'javascript',
-		'js'
+		'js',
+		'java',
+		'c#',
+		'c++',
 ];
 export const TOOLS = [
 		'docker',
 		'ci/cd',
-		'container'
+		'container',
+		'react',
+		'vue',
+		'angular',
 ];
 export const METHODS = [
 		'scrum',
@@ -37,5 +46,6 @@ export const METHODS = [
 		'test',
 		'tdd',
 		'bdd',
-		'design'
+		'design',
+		'time',
 ];

e241fdfe908f7191acb39a68a360b099ee18acd1
kevinflynn <flynny85@gmail.com>
2023-03-24 12:36:54 +0000

    add splash page content wip

--- client/src/lang/en-gb.js
+++ client/src/lang/en-gb.js
@@ -11,6 +11,11 @@ export const PASSWORD_PLACEHOLDER = "*******"
 export const NO_ACCOUNT = "Don't have an account?"
 export const HAVE_ACCOUNT = "Already have an account?"
 
+export const SPLASH_CONTENT_LOG = "Create daily short Logs to document your development progress"
+export const SPLASH_CONTENT_TAG = "Mention common processes, tools or methods to add specific #Tags"
+export const SPLASH_CONTENT_CUSTOM = "Custom #Tags are possible e.g. #new-thing"
+export const SPLASH_CONTENT_TIMELINE = "Search your Logs for the last time you used a specific process, tool or method"
+export const SPLASH_CONTENT_INSIGHT = "Have deep insight into your Logs over time using keywords or #Tags"
 
 
 export const LOG_PLACEHOLDERS = [

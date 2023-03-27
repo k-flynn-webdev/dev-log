6d622341fd3d8398a94a29132fd3f1682aca7dab
kevinflynn <flynny85@gmail.com>
2023-03-27 14:33:17 +0100

    tweaks on seeds

--- api/seeds/1_addTags.js
+++ api/seeds/1_addTags.js
@@ -1,7 +1,10 @@
 const USER_ID = 1
-const ENUM_TOOLS = "tools"
+const ENUM_TOOL = "tool"
 const ENUM_LANGUAGE = "language"
-const ENUM_METHODS = "methods"
+const ENUM_METHOD = "method"
+const ENUM_FRAMEWORK = "framework"
+const ENUM_LIBRARY = "library"
+const ENUM_PROCESSOR = "processor"
 
 /**
  * @param { import("knex").Knex } knex
@@ -18,18 +21,22 @@ export async function seed(knex) {
     { value: 'java', type: ENUM_LANGUAGE, user_id: USER_ID },
     { value: 'c#', type: ENUM_LANGUAGE, user_id: USER_ID },
     { value: 'c++', type: ENUM_LANGUAGE, user_id: USER_ID },
-    { value: 'docker', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'cicd', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'container', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'react', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'vue', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'angular', type: ENUM_TOOLS, user_id: USER_ID },
-    { value: 'scrum', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'agile', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'test', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'tdd', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'bdd', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'design', type: ENUM_METHODS, user_id: USER_ID },
-    { value: 'time', type: ENUM_METHODS, user_id: USER_ID },
+    { value: 'css', type: ENUM_LANGUAGE, user_id: USER_ID },
+    { value: 'html', type: ENUM_LANGUAGE, user_id: USER_ID },
+    { value: 'docker', type: ENUM_TOOL, user_id: USER_ID },
+    { value: 'cicd', type: ENUM_TOOL, user_id: USER_ID },
+    { value: 'container', type: ENUM_TOOL, user_id: USER_ID },
+    { value: 'react', type: ENUM_LIBRARY, user_id: USER_ID },
+    { value: 'vue', type: ENUM_FRAMEWORK, user_id: USER_ID },
+    { value: 'angular', type: ENUM_FRAMEWORK, user_id: USER_ID },
+    { value: 'scrum', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'agile', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'test', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'tdd', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'bdd', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'design', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'time', type: ENUM_METHOD, user_id: USER_ID },
+    { value: 'webpack', type: ENUM_PROCESSOR, user_id: USER_ID },
+    { value: 'vite', type: ENUM_TOOL, user_id: USER_ID },
   ]);
 };

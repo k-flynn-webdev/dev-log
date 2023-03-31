fd1ea7643b03a5cb8969b9263e4f51a93228a07a
kevinflynn <flynny85@gmail.com>
2023-03-31 18:27:25 +0100

    tags now responsive

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -3,29 +3,20 @@ import Tags from "./Tags"
 import LogDate from "./LogDate"
 import LogMenu from "./LogMenu"
 
-export const getMarginSpace = log => {
-  const hasTags = log.tags.length ? 1 : 0
-  const rows = Math.ceil(log.tags.length / 3)
-  const result = (hasTags + rows) * 2
-  return `mb-${log.tags.length ? result + 1 : 2}`
-}
-
 function Log({ log }) {
   return (
-    <div className={`log ${getMarginSpace(log)}`} title={log.value}>
-      <div>
+    <div>
+      <div className={`log ${log.tags.length ? "" : "mb-3"}`} title={log.value}>
         <div className="log-text">
           <p>{log.value}</p>
-        </div>
-
-        <div className="log-tags">
-          <Tags tags={log.tags} />
         </div>
 
         <LogDate created_at={log.created_at} />
 
         <LogMenu />
       </div>
+
+      <Tags tags={log.tags} />
     </div>
   )
 }

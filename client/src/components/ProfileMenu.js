bed9275e5d667fae23e91d5a76dff0d5ad4e1fcf
kevinflynn <flynny85@gmail.com>
2023-04-06 11:21:25 +0100

    cleanup on look

--- client/src/components/ProfileMenu.js
+++ client/src/components/ProfileMenu.js
@@ -23,10 +23,29 @@ function ProfileMenu({ userName, onClick }) {
 
   return (
     <div className="profile-menu">
-      <h1>{userName}</h1>
-      <button onClick={logoutUser}>{LOGOUT}</button>
-      <br />
-      <button onClick={onClick}>X</button>
+      <div className="profile-menu__header">
+        <h1 className="title">{userName}</h1>
+        <button className="close-button" onClick={onClick}>
+          <svg
+            xmlSpace="preserve"
+            fillRule="evenodd"
+            strokeLinejoin="round"
+            strokeMiterlimit="2"
+            clipRule="evenodd"
+            viewBox="0 0 16 16"
+            className="icon:2 icon-close"
+          >
+            <path d="M5.9219 6.1694h-5.11c-.22 0-.39.176-.39.392v3.217c0 .216.17.391.39.391h5.11v5.109c0 .216.17.391.39.391h3.22c.21 0 .39-.175.39-.391v-5.109h5.11c.21 0 .39-.175.39-.391v-3.217c0-.216-.18-.392-.39-.392h-5.11v-5.108c0-.216-.18-.392-.39-.392h-3.22c-.22 0-.39.176-.39.392v5.108Z" />
+          </svg>
+        </button>
+      </div>
+
+      <div className="profile-menu__content">
+        <div>11</div>
+        <button className="quit-button" onClick={logoutUser}>
+          {LOGOUT}
+        </button>
+      </div>
     </div>
   )
 }

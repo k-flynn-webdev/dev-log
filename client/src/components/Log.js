26b9c55057742e28ab5e6e8cdf32881e5e1a517e
kevinflynn <flynny85@gmail.com>
2023-04-18 18:47:28 +0100

    add log patcher function

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -4,15 +4,31 @@ import LogText from "./LogText"
 import LogEdit from "./LogEdit"
 import LogDate from "./LogDate"
 import LogMenu from "./LogMenu"
+import { useState } from "react"
 
 function Log({ log }) {
+  const [isOpen, setOpen] = useState(false)
+  const classNames = `log ${log.tags.length ? "" : "mb-3"} ${
+    isOpen ? "open" : ""
+  }`
+
+  const handleMenuClick = () => {
+    setOpen(!isOpen)
+  }
+
   return (
     <div>
+      <div className={classNames} title={log.value}>
+        <>
+          {isOpen ? (
+            <LogEdit log={log} handleIsOpen={handleMenuClick} />
+          ) : (
             <LogText log={log} />
           )}
+        </>
         <LogDate log={log} />
         <LogMenu isOpen={isOpen} handleIsOpen={handleMenuClick} />
-        </div>
+      </div>
 
       <Tags log={log} />
     </div>
---
+++ client/src/components/LogEdit.js
@@ -0,0 +1,71 @@
+import * as React from "react"
+import { useState } from "react"
+import FormInput from "./FormInput"
+import { useDispatch } from "react-redux"
+import { patchLogAPI, patchLog } from "../store/slices/logs"
+
+import { clearError } from "../store/slices/error"
+
+import {
+  LOG_INPUT_MIN_LENGTH,
+  LOG_INPUT_MAX_LENGTH,
+  isValidLogLength,
+} from "../helpers/log-input"
+
+import { UPDATE, DELETE } from "../lang/en-gb"
+
+function LogEdit({ log, handleIsOpen }) {
+  const dispatch = useDispatch()
+  const [logValue, setLogValue] = useState(log.value)
+  const isValid = isValidLogLength(logValue.length) && logValue !== log.value
+
+  const handleChange = event => {
+    setLogValue(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (log.value === logValue) return
+
+    dispatch(clearError())
+    dispatch(patchLog({ id: log.id, value: logValue }))
+    dispatch(patchLogAPI({ id: log.id, value: logValue }))
+      .unwrap()
+      .then(() => handleIsOpen(false))
+  }
+
+  return (
+    <div className="log-edit">
+      <form onSubmit={handleSubmit} className="log-edit__form">
+        <FormInput
+          className="log-edit__form-input"
+          placeholder={logValue}
+          value={logValue}
+          type="text"
+          min={LOG_INPUT_MIN_LENGTH}
+          max={LOG_INPUT_MAX_LENGTH}
+          isValid={isValid}
+          onChange={handleChange}
+        />
+        <div className="log-edit__form__controls">
+          <button
+            className="log-edit__form__controls-button primary"
+            type="submit"
+            disabled={!isValid}
+          >
+            {UPDATE}
+          </button>
+          <button
+            className="log-edit__form__controls-button warning"
+            type="submit"
+            disabled={!isValid}
+          >
+            {DELETE}
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LogEdit
